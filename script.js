76d4a72746870e1ddba8a2f089d4e0fb815274ae
Yogesh Yadav <yadavyogesh404@gmail.com>
2023-07-23 11:47:11 +0530

    sun-phase1

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/************************************************************************************* *
+                                      navbar
+***************************************************************************************/
+
 var hamburger = document.getElementsByClassName("hidden")[0];
 var navUl = document.getElementsByClassName("navUl")[0];
 var leftBar = document.getElementsByClassName("left-bar")[0];
@@ -16,93 +20,92 @@ function buttonClick() {
   }
 }
 
-// slider less go
-let slideIndex = 1;
-showSlides(slideIndex);
+/************************************************************************************* *
+                                      slider
+***************************************************************************************/
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+  const bullets = document.getElementsByClassName("bullet");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+
+  slides[slideIndex - 1].style.opacity = 1;
+
+  for (let i = 0; i < bullets.length; i++) {
+    bullets[i].classList.remove("active");
+  }
+
+  bullets[slideIndex - 1].classList.add("active");
 }
 
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+  showSlide(slideIndex = n);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+function plusSlides(n) {
+  showSlide(slideIndex += n);
+}
+
+setInterval(() => {
+  plusSlides(1);
+}, 6000); // Change slide every 5 seconds
+
+
+/************************************************************************************* *
+                                      expand and collapse
+***************************************************************************************/
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+
+  var acc = document.getElementsByClassName("accordion");
+  var panel = document.getElementsByClassName('panel');
+  
+  for (var i = 0; i < acc.length; i++) {
+      acc[i].onclick = function() {
+          var setClasses = !this.classList.contains('active');
+          setClass(acc, 'active', 'remove');
+          setClass(panel, 'show', 'remove');
+  
+          if (setClasses) {
+              this.classList.toggle("active");
+              this.nextElementSibling.classList.toggle("show");
+          }
+      }
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+  
+  function setClass(els, className, fnName) {
+      for (var i = 0; i < els.length; i++) {
+          els[i].classList[fnName](className);
+      }
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
-var acc = document.getElementsByClassName("accordion");
-var panel = document.getElementsByClassName("panel");
-
-for (var i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    var setClasses = !this.classList.contains("active");
-    setClass(acc, "active", "remove");
-    setClass(panel, "show", "remove");
-
-    if (setClasses) {
-      this.classList.toggle("active");
-      this.nextElementSibling.classList.toggle("show");
-    }
-  };
-}
-
-function setClass(els, className, fnName) {
-  for (var i = 0; i < els.length; i++) {
-    els[i].classList[fnName](className);
-  }
-}
-
-
-
-var acc = document.getElementsByClassName("accordion");
-var panel = document.getElementsByClassName('panel');
-
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function() {
-        var setClasses = !this.classList.contains('active');
-        setClass(acc, 'active', 'remove');
-        setClass(panel, 'show', 'remove');
-
-        if (setClasses) {
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
-    }
-}
-
-function setClass(els, className, fnName) {
-    for (var i = 0; i < els.length; i++) {
-        els[i].classList[fnName](className);
-    }
-}
+  
+  });
 
 
 /* **********************************************************************************
                                     footer
 ***********************************************************************************/
-// Function to toggle the visibility of footer links when header is clicked
+
 function toggleFooterLinks(headerElement) {
-  const linksList = headerElement.nextElementSibling;
-  linksList.style.display =
-    linksList.style.display === 'none' ? 'block' : 'none';
+  const linksList = headerElement.nextElementSibling.querySelector('.categoryList');
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth <= 768) {
+    linksList.classList.toggle('show-links');
+  }
 }
 
 // Attach click event listeners to each header element
@@ -113,3 +116,14 @@ footerHeadings.forEach((heading) => {
   });
 });
 
+// Event listener to close the footer links when the window size is changed
+window.addEventListener('resize', () => {
+  const windowWidth = window.innerWidth;
+  const linksLists = document.querySelectorAll('.categoryList');
+
+  if (windowWidth > 768) {
+    linksLists.forEach((linksList) => {
+      linksList.classList.remove('show-links');
+    });
+  }
+});
